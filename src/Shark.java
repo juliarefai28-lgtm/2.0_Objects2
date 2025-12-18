e05c6bdc299d80dba615504be34578eebdab6df1
juliarefai28-lgtm <julia_refai28@milton.edu>
2025-12-18 09:30:52 -0500

    Starting aquariam project

---
+++ src/Shark.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+
+public class Shark {
+    import java.awt.*;
+
+    public class Shark {
+
+        //VARIABLE DECLARATION SECTION
+        //Here's where you state which variables you are going to use.
+        public String name;                //holds the name of the hero
+        public int xpos;                //the x position
+        public int ypos;                //the y position
+        public int dx;                    //the speed of the hero in the x direction
+        public int dy;                    //the speed of the hero in the y direction
+        public int width;
+        public int height;
+        public boolean isAlive;//a boolean to denote if the hero is alive or dead.
+        public Rectangle hitbox;
+
+
+
+        // METHOD DEFINITION SECTION
+
+        // Constructor Definition
+        // A constructor builds the object when called and sets variable values.
+
+
+        //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+        // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+        public Shark (int pXpos, int pYpos) {
+            xpos = pXpos;
+            ypos = pYpos;
+            dx = 10;
+            dy = 4;
+            width = 60;
+            height = 60;
+            isAlive = true;
+            hitbox= new Rectangle(xpos,ypos,width,height);
+
+
+        } // constructor
+
+        //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+        public void move() {
+
+            xpos = xpos + dx;
+            ypos = ypos + dy;
+            if (xpos >= 1000 - width) {//bounce of left wall
+                dx = -dx;
+            }
+            if (ypos >= 700 - height) {//bounce of bottom wall
+                dy = -dy;
+            }
+
+            if (xpos<0){dx=-dx;}
+            if(ypos<0){dy=-dy;}
+
+            hitbox= new Rectangle(xpos,ypos,width,height);
+        }
+
+    }
+}
